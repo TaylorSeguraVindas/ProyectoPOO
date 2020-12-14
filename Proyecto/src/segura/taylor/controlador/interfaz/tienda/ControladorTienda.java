caac07585d7abfe7c3bb4c06e337e0b5cae5a229
TaylorSeguraVindas <tsegurav@ucenfotec.ac.cr>
2020-12-13 19:47:27 -0600

    Mostrar albumes en tienda

--- Proyecto/src/segura/taylor/controlador/interfaz/tienda/ControladorTienda.java
+++ Proyecto/src/segura/taylor/controlador/interfaz/tienda/ControladorTienda.java
@@ -1,71 +1,46 @@
 package segura.taylor.controlador.interfaz.tienda;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
-import segura.taylor.bl.entidades.ListaReproduccion;
-import segura.taylor.controlador.ControladorGeneral;
-
-import java.util.List;
 
 public class ControladorTienda {
-    public ScrollPane contenedorFlowPane;
-    public FlowPane flowPaneContenido;
+    public VBox contenedorPrincipal;
 
     public void initialize() {
         mostrarListasReproduccion();
-
-        flowPaneContenido.prefWidthProperty().bind(contenedorFlowPane.widthProperty()); //Expandir
     }
 
-    private void mostrarListasReproduccion() {
-        flowPaneContenido.getChildren().clear();    //Limpiar contenido
-
-        List<ListaReproduccion> listasReproduccion = ControladorGeneral.instancia.getGestor().listarListasReproduccion();
-
-        for (ListaReproduccion listaReproduccion : listasReproduccion) {
-            crearCartaListaReproduccion(listaReproduccion.getId(), listaReproduccion.getImagen(), listaReproduccion.getNombre(), listaReproduccion.getDescripcion());
-        }
+    private void limpiarPantalla() {
+        contenedorPrincipal.getChildren().clear();
     }
 
-    private void crearCartaListaReproduccion(int idLista, String imagen, String nombre, String descripcion) {
+    public void mostrarListasReproduccion() {
+        limpiarPantalla();
         try {
-            VBox nuevaCarta = FXMLLoader.load(getClass().getResource("../../../ui/ventanas/tienda/ElementoListaReproduccion.fxml"));
-
-            //Referencias a campos
-            ImageView imagenFondo = (ImageView) nuevaCarta.lookup("#imgFondo");
-            Label txtNombre = (Label) nuevaCarta.lookup("#txtNombre");
-            Label txtDescripcion = (Label) nuevaCarta.lookup("#txtDescripcion");
-
-            //Actualizar campos
-            if(!imagen.equals("")) {
-                imagenFondo.setImage(new Image(imagen));
-            }
-
-            txtNombre.setText(nombre);
-            txtDescripcion.setText(descripcion);
-
-            nuevaCarta.setOnMouseClicked( e -> {
-                mostrarDetalleListaReproduccion(idLista);
-            });
-
-            flowPaneContenido.getChildren().add(nuevaCarta);
+            VBox root = FXMLLoader.load(getClass().getResource("../../../ui/ventanas/tienda/VistaListasReproduccion.fxml"));
+            contenedorPrincipal.getChildren().add(root);
+            //Expandir
+            root.prefWidthProperty().bind(contenedorPrincipal.widthProperty());
+            root.prefHeightProperty().bind(contenedorPrincipal.heightProperty());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void mostrarDetalleListaReproduccion(int idLista) {
-        ControladorInfoListaReproduccion.idListaSeleccionada = idLista;
-
-        if(ControladorGeneral.instancia.getGestor().usuarioIngresadoEsAdmin()) {
-            ControladorGeneral.refVentanaPrincipalAdmin.mostrarInfoListaReproduccion();
-        } else {
-            //TODO Hacer lo mismo pero para cliente
+    public void mostrarAlbumes() {
+        limpiarPantalla();
+        try {
+            VBox root = FXMLLoader.load(getClass().getResource("../../../ui/ventanas/tienda/VistaAlbumes.fxml"));
+            contenedorPrincipal.getChildren().add(root);
+            //Expandir
+            root.prefWidthProperty().bind(contenedorPrincipal.widthProperty());
+            root.prefHeightProperty().bind(contenedorPrincipal.heightProperty());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void mostrarCanciones() {
+        //TODO mostrar canciones
     }
 }
---
+++ Proyecto/src/segura/taylor/controlador/interfaz/tienda/ControladorVistaAlbumes.java
@@ -0,0 +1,68 @@
+package segura.taylor.controlador.interfaz.tienda;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import segura.taylor.bl.entidades.Album;
+import segura.taylor.bl.entidades.ListaReproduccion;
+import segura.taylor.controlador.ControladorGeneral;
+
+import java.util.List;
+
+public class ControladorVistaAlbumes {
+    public ScrollPane contenedorFlowPane;
+    public FlowPane flowPaneContenido;
+
+    public void initialize() {
+        mostrarListasReproduccion();
+        flowPaneContenido.prefWidthProperty().bind(contenedorFlowPane.widthProperty()); //Expandir
+    }
+
+    private void mostrarListasReproduccion() {
+        flowPaneContenido.getChildren().clear();    //Limpiar contenido
+
+        List<Album> albumes = ControladorGeneral.instancia.getGestor().listarAlbunes();
+
+        for (Album album : albumes) {
+            crearCartaListaReproduccion(album.getId(), album.getImagen(), album.getNombre());
+        }
+    }
+
+    private void crearCartaListaReproduccion(int idAlbum, String imagen, String nombre) {
+        try {
+            VBox nuevaCarta = FXMLLoader.load(getClass().getResource("../../../ui/ventanas/tienda/ElementoAlbum.fxml"));
+
+            //Referencias a campos
+            ImageView imagenFondo = (ImageView) nuevaCarta.lookup("#imgFondo");
+            Label txtNombre = (Label) nuevaCarta.lookup("#txtNombre");
+
+            //Actualizar campos
+            if(!imagen.equals("")) {
+                imagenFondo.setImage(new Image(imagen));
+            }
+
+            txtNombre.setText(nombre);
+            nuevaCarta.setOnMouseClicked( e -> {
+                mostrarDetalleAlbum(idAlbum);
+            });
+
+            flowPaneContenido.getChildren().add(nuevaCarta);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void mostrarDetalleAlbum(int idAlbum) {
+        ControladorInfoListaReproduccion.idListaSeleccionada = idAlbum;
+
+        if(ControladorGeneral.instancia.getGestor().usuarioIngresadoEsAdmin()) {
+            //ControladorGeneral.refVentanaPrincipalAdmin.mostrarInfoListaReproduccion();
+        } else {
+            //TODO Hacer lo mismo pero para cliente
+        }
+    }
+}
